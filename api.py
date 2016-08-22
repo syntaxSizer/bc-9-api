ca9f452498659a92782fcd0975a3f0a91ae660f7
syntaxSizer <kakuma4never@gmail.com>
2016-08-22 21:33:55 +0300

    arrange data display in a table layout

--- api.py
+++ api.py
@@ -1,30 +1,30 @@
-import urllib3,urllib2 ,json
+import urllib3
+import json
 
 
-city_name = ['Nairobi','Lagos', 'London', 'Khartom', 'Tel aviv','Kampala','Bagdad', 'Cairo']
+city_name = ['Nairobi', 'Lagos', 'London', 'Khartom',
+             'Tel aviv', 'Kampala', 'Bagdad', 'Cairo', 'Kigali']
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+API = "2bc3e79bb974a007818864813f53fd35"
+
 
 def get_details(city_name):
+    '''
+    program that desplay weather condition
+    '''
 
-	'''
-	program that desplay weather condition
-	'''
-
-	http = urllib3.PoolManager()
-	base_url = 'http://api.openweathermap.org/data/2.5/weather'
-	api = "2bc3e79bb974a007818864813f53fd35"
-	request = http.request( 'GET',base_url,
-		fields = {
-		'q': city_name,
-		'units': 'metric',
-		'appid': api
-		
-		})
-	response = json.loads(request.data.decode('utf-8'))
-	# format the output
-	return '{} {}'.format(str(response ['weather'][0]['description']).ljust(15), str(response['main']['temp'] ).rjust(15))
-print (' descriptions          temperature       city  \n ' + "=" *50)	
+    http = urllib3.PoolManager()
+    request = http.request('GET', BASE_URL,
+                           fields={
+                               'q': city_name,
+                               'units': 'metric',
+                               'appid': API
+                           })
+    response = json.loads(request.data.decode('utf-8'))
+    # format the output
+    return '{} {}'.format(str(response['weather'][0]
+    ['description']).ljust(20), str(response['main']['temp']).ljust(20))
+print 'descriptions'.ljust(20), 'temperature'.ljust(20), 'city'.ljust(20),'\n' + "=" * 50
 # loop in the cites
-for city in city_name:    
-	print   get_details(city), city.rjust(15 )
-
-
+for city in city_name:
+    print get_details(city), city.ljust(20)
