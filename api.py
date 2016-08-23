0e13e363d8287140df465e73e5a4838ddaf49c55
syntaxSizer <kakuma4never@gmail.com>
2016-08-23 07:07:35 +0300

    fix display format

--- api.py
+++ api.py
@@ -23,8 +23,8 @@ def get_details(city_name):
     response = json.loads(request.data.decode('utf-8'))
     # format the output
     return '{} {}'.format(str(response['weather'][0]
-    ['description']).ljust(20), str(response['main']['temp']).ljust(20))
-print 'descriptions'.ljust(20), 'temperature'.ljust(20), 'city'.ljust(20),'\n' + "=" * 50
+                              ['description']).ljust(20), str(response['main']['temp']).ljust(20))
+print 'descriptions'.ljust(20), 'temperature'.ljust(20), 'city'.ljust(20), '\n' + "=" * 50
 # loop in the cites
 for city in city_name:
     print get_details(city), city.ljust(20)
